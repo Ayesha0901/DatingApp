96b2deeb87e53f1216538b5a47712cb31203bf07
Ayesha0901 <ayshaysh842@gmail.com>
2023-12-17 12:08:34 +0530

    End of section 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,49 +1,37 @@
-﻿using API.Data;
+﻿using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace API.Controllers
 {
     [Authorize]
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
+        
+        private readonly IUserRepository _userRepository ;
+        private readonly IMapper _mapper;
 
-        public UsersController(DataContext context)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
+            _mapper = mapper;
+            _userRepository = userRepository;
         }
 
-        [AllowAnonymous]
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            var users = await _context.Users.ToListAsync();
-            return users;
+            var users = await _userRepository.GetMemberAsync();
+            
+            return Ok(users);
         }
 
-        [HttpGet("{id}")]
-        [AllowAnonymous] // Allow unauthenticated access to this specific action
-        public async Task<ActionResult<AppUser>> GetUser(int id)
+        [HttpGet("{username}")] // Allow unauthenticated access to this specific action
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return user;
-        }
-
-        [HttpGet("authenticated")]
-        public ActionResult<string> GetAuthenticated()
-        {
-            // Only authenticated users can access this action
-            return "This is an authenticated endpoint.";
+           return await _userRepository.GetMemberAsync(username);
         }
     }
 }
